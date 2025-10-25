e309c9e502a106417eb794de67508b27f27375f9
Ria Saraswat <99956952+Dare-TechieZ@users.noreply.github.com>
2025-10-25 20:08:59 +0530

    Create SearchIN2DMatrix.cpp

---
+++ SearchIN2DMatrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int i=0,j=col-1;
+        while(i<row and j>=0){
+            if(target==matrix[i][j]){
+                return true;
+            }
+            else if(target<matrix[i][j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+};
