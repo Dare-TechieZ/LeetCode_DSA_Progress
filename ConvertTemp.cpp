86c58a931d48851fcff28c3130374e0b2f0de1dd
Ria Saraswat <99956952+Dare-TechieZ@users.noreply.github.com>
2025-10-25 20:04:07 +0530

    Create ConvertTemp.cpp

---
+++ ConvertTemp.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15,fahrenheit = celsius * 1.80 + 32.00;
+        return {kelvin,fahrenheit};
+    }
+};
